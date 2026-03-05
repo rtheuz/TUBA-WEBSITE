7bd1ae44e9c48851fd3004427b05c7d733e2e2ac
Matheus de Souza Rodrigues <matheus.761730@graduacao.fsa.br>
2026-03-05 17:32:48 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/TUBA-WEBSITE",
+  //basePath: "/TUBA-WEBSITE",
   images: {
     unoptimized: true,
   },
