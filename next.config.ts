35f390f86926f9d590bff5bcfab810af71545707
Matheus de Souza Rodrigues <matheus.761730@graduacao.fsa.br>
2026-03-05 14:35:27 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Site-Tuba",
+  basePath: "/TUBA-WEBSITE",
   images: {
     unoptimized: true,
   },
